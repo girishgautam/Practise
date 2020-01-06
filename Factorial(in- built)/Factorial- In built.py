50c802288f9672885bb0b693d2019a5504411761
Girish <59560747+girishgautam@users.noreply.github.com>
2020-01-06 21:48:30 +0000

    For loop method added

--- Factorial(in- built)/Factorial- In built.py
+++ Factorial(in- built)/Factorial- In built.py
@@ -19,11 +19,18 @@
 #     print('Factorial of', i, 'is', fact(i))
 
 # For loop
+def fact (num) :
+    sum = 1
+    for i in range(1, num + 1):
+        if i == 0:
+            sum = 1
+        if i == 1:
+            sum = 1
+        else:
+            sum = sum * i
+    return sum
 
 
 
-
-
-
-for i in range(6):
+for i in range(7):
     print('Factorial of', i, 'is', fact(i))

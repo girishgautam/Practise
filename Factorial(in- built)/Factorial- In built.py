9e28399b89e22abc2000e1f6b32840ccd7591a32
Girish Joshi <girishgautam@gmail.com>
2020-01-11 13:54:52 +0000

    Second commit

--- Factorial(in- built)/Factorial- In built.py
+++ Factorial(in- built)/Factorial- In built.py
@@ -18,23 +18,23 @@
 # for i in range(6):
 #     print('Factorial of', i, 'is', fact(i))
 
-# For loop
-def fact (num) :
-    sum = 1
-    for i in range(1, num + 1):
-        sum = sum * i
-    return sum
-
-# Using while loop
-
-# def fact (num):
+# # For loop
+# def fact (num) :
 #     sum = 1
-#     # i = 1
-#     while num > 0:
-#         sum = sum * num
-#         num -= 1
+#     for i in range(1, num + 1):
+#         sum = sum * i
 #     return sum
 #
+# # Using while loop
 #
-for i in range(5):
-    print('Factorial of', i, 'is', fact(i))
+# # def fact (num):
+# #     sum = 1
+# #     # i = 1
+# #     while num > 0:
+# #         sum = sum * num
+# #         num -= 1
+# #     return sum
+# #
+# #
+# for i in range(5):
+#     print('Factorial of', i, 'is', fact(i))
